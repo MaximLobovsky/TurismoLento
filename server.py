4493f4e39e71525b27b64e5183c1b2601f3e39dd
Maxim Lobovsky <mex.lobo@mail.ru>
2021-03-18 10:58:25 +0100

    aggiunto il prototipo di menu laterale

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+from markupsafe import escape
+from flask import render_template
+
+@app.route('/')
+def hello():
+    return render_template('index.html', num_element_lista = range(10))
+
+
