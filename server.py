b1759a6e3fc98c8a546796b4d182dcadfebe6f1f
Maxim Lobovsky <mex.lobo@mail.ru>
2021-03-30 01:06:06 +0200

    aggiunta roba

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,4 +9,12 @@ from flask import render_template
 def hello():
     return render_template('index.html', num_element_lista = range(50))
 
+@app.route('/calculus', methods=['post'])
+def recive_string():
+    myString = request.form['mia_stringa']
+    half=len(myString)//2
+    return myString[0:half]
 
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=1234, debug=True)
