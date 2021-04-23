df93ba02735a73b9c3cc84794bf3acfb1e34b60e
Maxim Lobovsky <mex.lobo@mail.ru>
2021-04-24 01:22:06 +0200

    aggiunta nav bar, suddivisione in colonne, robba

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 'flask è un framework, ti dà una serie di strumenti basilari per risolvere problemi già risolti da altri.'
 'jinja2 è un linguaggio di templating'
 
@@ -15,7 +14,7 @@ def get_sheet_values():
     # Create scope
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     # create some credential using that scope and content of keys.json
-    creds = ServiceAccountCredentials.from_json_keyfile_name('turismo lento-0d0d635cf9c6.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('chiavi.json', scope)
     # create gspread authorize using that credential
     client = gspread.authorize(creds)
     # Now will can access our google sheets we call client.open on StartupName
@@ -31,19 +30,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-
     elements = get_sheet_values()
     # with open('test.json', 'w') as f:
     #     json.dump(elements, f, indent=4)
 
-    return render_template('index.html', num_element_lista=range(len(elements) - 1), data=elements)
+    return render_template('localita.html', num_element_lista=range(len(elements) - 1), data=elements)
+
 
 @app.route('/calculus', methods=['post'])
 def recive_string():
     myString = request.form['mia_stringa']
     half = len(myString) // 2
     return myString[0:half]
-
 
 
 if __name__ == "__main__":
