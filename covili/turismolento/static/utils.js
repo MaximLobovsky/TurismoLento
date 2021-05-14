2d0296e2cb0104318428e45770a73e8c2a0ff845
liuk3 <luca.covili@fermi.mo.it>
2021-05-14 10:54:41 +0200

    map stuff v2

--- covili/turismolento/static/utils.js
+++ covili/turismolento/static/utils.js
@@ -1,23 +1,30 @@
 // JavaScript Document
+
+
+//create the map in the div with id "map"
 function CreateMap(){
-    let map = L.map("map").setView([41.902480, 12.496355], 6);
+    var map = L.map("map").setView([41.902480, 12.496355], 6);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    layers = L.layerGroup();
     return map;
 };
 
+//place a single marker
 function PlaceMarker(map, N, E, description, link = ''){
-	L.marker([N, E]).addTo(map)
-        .bindPopup()
+	L.marker([N, E]).addTo(layers)
+      .bindPopup()
 	    .setPopupContent(description + '<br><a href=' + link.toString() + ' target="_blank" rel="noopener noreferrer">Visita il sito</a>');
 
 };
 
+//reset map view
 function setDefaultView(map){
     map.setView([41.902480, 12.496355],6);
 }
 
-function PlaceMarkers(map, places, descriptions, links){
+//place markers with information in the lists
+function PlaceMarkers(map, coordinates, descriptions, links){
     let place;
     let counter = 0;
     for (place of coordinates){
@@ -27,6 +34,25 @@ function PlaceMarkers(map, places, descriptions, links){
         }
     }
     setDefaultView(map);
+    map.addLayer(layers);
+}
+
+function ClearMap(){
+  layers.clearLayers();
+}
+
+// reset map id div
+function NewMap(places_index, coordinates, descriptions, links){
+  ClearMap();
+  let i = 0;
+  let j = 0;
+  for(i = 0; i<descriptions.length; i++){
+    for(j = 0; j< places_index.length; j++){
+      if(i == j){
+        PlaceMarker(map, coordinates[i][0], coordinates[i][1], descriptions[i], links[i]);
+      }
+    }
+  }
 }
 
 
@@ -42,13 +68,13 @@ function PlaceMarkers(map, places, descriptions, links){
 
 
 // Javascript Lobovsky
-
+/*
 function play_sound(soundobj) {
     let thissound = document.getElementById(soundobj);
     thissound.currentTime = 0;
     thissound.play();
   }
-  
+  */
   
   
   function regione_filter() {
@@ -97,7 +123,7 @@ function play_sound(soundobj) {
   
   // Ricerca
   
-  function ricerca(tab, lists) {
+  function ricerca(tab, lists, coordinates, descriptions, links) {
     let input;
     let filtro;
     let lista;
@@ -105,6 +131,7 @@ function play_sound(soundobj) {
     let x;
     let i;
     let testo;
+    let index_founds = [];
     input = document.getElementById(tab);
     filtro = input.value.toUpperCase();
     lista = document.getElementById('list_container');
@@ -114,8 +141,11 @@ function play_sound(soundobj) {
       testo = x.textContent || x.innerText;
       if (testo.toUpperCase().indexOf(filtro) > -1) {
         voci[i].style.display = "";
+        index_founds.push(i);
       } else {
         voci[i].style.display = "none";
       }
     }
+    NewMap(index_founds, coordinates, descriptions, links);
   }
+
