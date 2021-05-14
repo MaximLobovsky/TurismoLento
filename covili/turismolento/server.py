792cde5e04e152e776083273e8e3f855d38ef740
liuk3 <70254810+liuk3@users.noreply.github.com>
2021-05-14 18:30:18 +0200

    mappa funzionante ed interattiva con la barra di ricerca

--- covili/turismolento/server.py
+++ covili/turismolento/server.py
@@ -105,5 +105,6 @@ def gallery():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=1234, debug=True)
+    # app.run(host='127.0.0.1', port=1234, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
     
--- covili/turismolento/static/utils.js
+++ covili/turismolento/static/utils.js
@@ -11,7 +11,7 @@ function CreateMap(){
 };
 
 //place a single marker
-function PlaceMarker(map, N, E, description, link = ''){
+function PlaceMarker(map = 0, N, E, description, link = ''){
 	L.marker([N, E]).addTo(layers)
       .bindPopup()
 	    .setPopupContent(description + '<br><a href=' + link.toString() + ' target="_blank" rel="noopener noreferrer">Visita il sito</a>');
@@ -21,7 +21,7 @@ function PlaceMarker(map, N, E, description, link = ''){
 //reset map view
 function setDefaultView(map){
     map.setView([41.902480, 12.496355],6);
-}
+};
 
 //place markers with information in the lists
 function PlaceMarkers(map, coordinates, descriptions, links){
@@ -48,24 +48,13 @@ function NewMap(places_index, coordinates, descriptions, links){
   let j = 0;
   for(i = 0; i<descriptions.length; i++){
     for(j = 0; j< places_index.length; j++){
-      if(i == j){
+      if(i == places_index[j]){
         PlaceMarker(map, coordinates[i][0], coordinates[i][1], descriptions[i], links[i]);
       }
     }
   }
+  map.addLayer(layers);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Javascript Lobovsky
 /*
@@ -75,54 +64,64 @@ function play_sound(soundobj) {
     thissound.play();
   }
   */
-  
-  
+
+
   function regione_filter() {
     lista = document.getElementById('selezione_regioni');
   }
-  
-  let checkbox_toggle = () => {
+
+  let checkbox_toggle = (coordinates, descriptions, links) => {
     // Get the checkbox
     let checkBox = document.getElementById("enable_regione_search");
     // Get the output text
     let coso = document.getElementById("elenco_regioni");
     let voci;
+    let index_founds = [];
+    let i;
     voci = document.getElementsByClassName('single_list_item');
-  
+
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true) {
       coso.style.display = "block";
-  
+      discriminazione_regione(coordinates, descriptions, links);
     } else {
       coso.style.display = "none";
-      for (let voce of voci) 
-      {
-        voce.style.display = "";
+      // for (let voce of voci)
+      // {
+      for(i = 0; i< voci.length; i++) {
+        voci[i].style.display = "";
+        index_founds.push(i);
       }
+      // }
     }
-  
+    NewMap(index_founds, coordinates, descriptions, links);
   }
-  
-  
-  
-  function discriminazione_regione() {
+
+
+
+  function discriminazione_regione(coordinates, descriptions, links) {
     let voci;
+    let i;
+    let index_founds = [];
     voci = document.getElementsByClassName('single_list_item');
     let regione_selezionata = document.getElementById("elenco_regioni").value;
-    for (let voce of voci) //stessa cosa di Python, for element in array
-    {
-      if (regione_selezionata === voce.getAttribute('data-regione')) {
-        voce.style.display = "";
+    // for (let voce of voci) //stessa cosa di Python, for element in array
+    // {
+    for (i = 0; i< voci.length; i++){
+      if (regione_selezionata === voci[i].getAttribute('data-regione')) {
+        voci[i].style.display = "";
+        index_founds.push(i);
       } else {
-        voce.style.display = "none";
+        voci[i].style.display = "none";
       }
     }
+    NewMap(index_founds, coordinates, descriptions, links);
   }
-  
-  
-  
+
+
+
   // Ricerca
-  
+
   function ricerca(tab, lists, coordinates, descriptions, links) {
     let input;
     let filtro;
